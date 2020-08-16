10f9a5c4a79e714ea1cca1a67386c78372e25c91
teaganmach <49710698+teaganmach@users.noreply.github.com>
2020-08-15 17:26:03 -0700

    Add files via upload

--- Calendar.java
+++ Calendar.java
@@ -7,28 +7,36 @@ public class Calendar {
    private Scanner console;
    
    // store the hours + activities for the final calendar
-   private Map<String, Integer> finalCal;
+   private Map<String, Double> finalCal;
    
-   private List<String> activities;
+   private Random rand; 
    
    public Calendar(){
-   
+      finalCal = new HashMap<String, Double>();
+      rand = new Random();
    }
    
    // for random
-   public void create(List<String> list, int maxHours){
-      finalCal = new HashMap<String, Integer>();
-      Random rand = new Random();
-      int remaining = maxHours;
-      while (remaining != 0){
-         int hours = rand.nextInt(3) + 1;
+   public void create(List<String> list, double maxHours){
+      if (maxHours != 0){
+         double hours = (rand.nextInt(6) + 1) * 0.5;
          int x = rand.nextInt(list.size());
-         if (remaining - hours >= 0){
-            remaining = remaining - hours;
+         if (maxHours - hours >= 0){
             String act = list.get(x);
             finalCal.put(act, hours);
          }
+         create(list, maxHours - hours);
       }
+//       remaining = maxHours;
+//       while (remaining != 0){
+//          int hours = rand.nextInt(3) + 1;
+//          int x = rand.nextInt(list.size());
+//          if (remaining - hours >= 0){
+//             remaining = remaining - hours;
+//             String act = list.get(x);
+//             finalCal.put(act, hours);
+//          }
+//       }
    }
    public void print(){
       for (String x : finalCal.keySet()){
